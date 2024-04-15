21db8030efc4c7ba4c40ccd773f9d607f410c9b1
Ayato Shiga-Callaway <beer.shigach@gmail.com>
2024-04-15 18:11:55 +0100

    implimented process to update rapid shot interval and burst shot charge duration for both mage and sorcerer.

--- reframework/autorun/zoltraak.lua
+++ reframework/autorun/zoltraak.lua
@@ -9,16 +9,16 @@ local sdk = sdk
 local _characterManager
 local function GetCharacterManager()
     if not _characterManager then 
-        _characterManager = sdk.get_managed_singleton("app.CharacterManager") 
+        _characterManager = sdk.get_managed_singleton("app.CharacterManager")
     end
 	return _characterManager
 end
 
 local function _get_component(value, func, name)
     if not value then
-        local _parent_component = func()
-        if _parent_component then
-            value = _parent_component:call(name)
+        local this_ = func()
+        if this_ then
+            value = this_:call(name)
         end
     end
     return value
@@ -26,118 +26,144 @@ end
 
 local _humanParam
 local function GetHumanParam()
-    -- if not _humanParam then
-    --     local characterManager = GetCharacterManager()
-    --     if characterManager then
-    --         _humanParam = characterManager:get_HumanParam()
-    --     end
-    -- end
-    -- return _humanParam
     _humanParam = _get_component(_humanParam, GetCharacterManager, "get_HumanParam()")
     return _humanParam
 end
 
 local _jobParam
 local function GetJobParam()
-    if not _jobParam then
-        local humanParam = GetHumanParam()
-        if humanParam then
-            _jobParam = humanParam:get_Job()
-        end
-    end
+    _jobParam = _get_component(_jobParam, GetHumanParam, "get_Job()")
     return _jobParam
-    -- _jobParam_ = _get_component(_jobParam, GetHumanParam, "get_Job()")
-    -- return _jobParam
+end
+
+local _mageParam
+local function GetMageParam()
+    _mageParam = _get_component(_mageParam, GetJobParam, "get_Job03Param()")
+    return _mageParam
 end
 
 local _sorcererParam
-local function GetSorcererParam()
-    if not _sorcererParam then
-        local jobParam = GetJobParam()
-        if jobParam then
-            _sorcererParam = jobParam:get_Job06Param()
-        end
-    end
+local function GetSorcererParameter ()
+    _sorcererParam = _get_component(_sorcererParam, GetJobParam, "get_Job06Param()")
     return _sorcererParam
-    -- _sorcererParam = _get_component(_sorcererParam, GetJobParam, "get_Job06Param()")
-    -- return _sorcererParam
 end
 
-local _manualPlayerHuman
-local function GetManualPlayerHuman()
-    if not _manualPlayerHuman then
-        local characterManager = GetCharacterManager()
-        if characterManager then
-            _manualPlayerHuman = characterManager:get_ManualPlayerHuman()
-        end
-    end
-    return _manualPlayerHuman
+local _burstShotParameter
+local function GetBurstShotParameter()
+    _burstShotParameter = _get_component(_burstShotParameter, GetSorcererParameter, "get_BurstShotParam")
+    return _burstShotParameter
 end
 
-local _manualPlayer
-local function GetManualPlayer()
-    if not _manualPlayer then
-        local characterManager = GetCharacterManager()
-        if characterManager then
-            _manualPlayer = characterManager:get_ManualPlayer()
-        end
-    end
-    return _manualPlayer
+local _rapidShotParameter
+local function GetSorcererRapidShotParameter()
+    _rapidShotParameter = _get_component(_rapidShotParameter, GetSorcererParameter, "get_RapidShotParam")
+    return _rapidShotParameter
 end
 
-local _staminaManager
-local function GetStaminaManager()
-    if not _staminaManager then
-        local manualPlayer = GetManualPlayer()
-        if manualPlayer then
-            _staminaManager = manualPlayer:get_StaminaManager()
-        end
-    end
-    return _staminaManager
+local _mageRapidShotParameter
+local function GetMageRapidShotParameter()
+    _mageRapidShotParameter = _get_component(_mageRapidShotParameter, GetMageParam, "get_RapidShotParamProp")
+    return _mageRapidShotParameter
 end
 
-local _humanCommonActionCtrl
-local function GetHumanCommonActionCtrl()
-    local manualPlayerHuman = GetManualPlayerHuman()
-    if manualPlayerHuman then
-        if _humanCommonActionCtrl == nil then 
-            _humanCommonActionCtrl = manualPlayerHuman:get_HumanCommonActionCtrl()
-        end
-    end
-    return _humanCommonActionCtrl
+local _powerShotParameter
+local function GetHeavyShotParameter()
+    _powerShotParameter = _get_component(_powerShotParameter, GetMageParam, "get_BurstShotParamProp")
+    return _powerShotParameter
 end
+
+local function updateSorcererRapidShot()
+    _rapidShotParameter = GetSorcererRapidShotParameter()
+    if _rapidShotParameter then
+        _rapidShotParameter:set_field("_ComboRapidShotTime", 0.1) -- 0.28 as default
+    end
+end
+
+local function updateBurstShotParameter()
+    _burstShotParameter = GetBurstShotParameter()
+    if _burstShotParameter then
+        _burstShotParameter:set_field("_PrepareTime", 0.5) -- 1 as default
+    end
+end
+
+local function updateMageRapidShot()
+    _mageRapidShotParameter = GetMageRapidShotParameter()
+    if _mageRapidShotParameter then
+        _mageRapidShotParameter:set_field("_ComboRapidShotTime", 0.1)
+    end
+end
+
+local function updatePowerShotParameter()
+    _powerShotParameter = GetHeavyShotParameter()
+    if _powerShotParameter then
+        _powerShotParameter:set_field("_PrepareTime", 0.5)
+    end
+end
+
+-- local _manualPlayerHuman
+-- local function GetManualPlayerHuman()
+--     if not _manualPlayerHuman then
+--         local characterManager = GetCharacterManager()
+--         if characterManager then
+--             _manualPlayerHuman = characterManager:get_ManualPlayerHuman()
+--         end
+--     end
+--     return _manualPlayerHuman
+-- end
+
+-- local _manualPlayer
+-- local function GetManualPlayer()
+--     if not _manualPlayer then
+--         local characterManager = GetCharacterManager()
+--         if characterManager then
+--             _manualPlayer = characterManager:get_ManualPlayer()
+--         end
+--     end
+--     return _manualPlayer
+-- end
+
+-- local _staminaManager
+-- local function GetStaminaManager()
+--     if not _staminaManager then
+--         local manualPlayer = GetManualPlayer()
+--         if manualPlayer then
+--             _staminaManager = manualPlayer:get_StaminaManager()
+--         end
+--     end
+--     return _staminaManager
+-- end
+
+-- local _humanCommonActionCtrl
+-- local function GetHumanCommonActionCtrl()
+--     local manualPlayerHuman = GetManualPlayerHuman()
+--     if manualPlayerHuman then
+--         if _humanCommonActionCtrl == nil then 
+--             _humanCommonActionCtrl = manualPlayerHuman:get_HumanCommonActionCtrl()
+--         end
+--     end
+--     return _humanCommonActionCtrl
+-- end
 
 local function resetScript()
     _characterManager = nil
     _humanParam = nil
-    _manualPlayerHuman = nil
-    _humanCommonActionCtrl = nil
-    _manualPlayerHuman = nil
-    _staminaManager = nil
+    _jobParam = nil
+    _sorcererParam = nil
+    _mageParam = nil
+    _rapidShotParameter = nil
+    _burstShotParameter = nil
+    _mageRapidShotParameter = nil
+    _powerShotParameter = nil
 end
 
-local function processDeath(characterManager)
-    if not characterManager then return end
-    if not characterManager:get_IsManualPlayerDead() then return end
-    resetScript()
-end
-
-local function initScript(sorcererParam)
-    if not sorcererParam then return end
-    local common = sorcererParam:get_CommonParam()
-    if common then
-        local walking_rate = common:get_AddRateForPreparingSpellWhileWalking()
-        local v = walking_rate + 1
-        print(v)
+sdk.hook(sdk.find_type_definition("app.Player"):get_method(".ctor"),
+    function () end,
+    function (...)
+        resetScript()
+        updateSorcererRapidShot()
+        updateBurstShotParameter()
+        updateMageRapidShot()
+        updatePowerShotParameter()
+        return ...
     end
-end
-
-re.on_frame(function ()
-    local humanCommonActionCtrl = GetHumanCommonActionCtrl()
-    local characterManager = GetCharacterManager()
-    local manualPlayerHuman = GetManualPlayerHuman()
-    local staminaManager = GetStaminaManager()
-    local sorcererParam = GetSorcererParam()
-    initScript(sorcererParam)
-    processDeath(characterManager)
-end)
+)
